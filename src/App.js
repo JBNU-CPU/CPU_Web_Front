5e1d6b1b6d2e55432ea4e4f0842b79e6d8eeee91
김동준 <coticoger@Dongjun-MacBook.local>
2024-09-14 09:45:40 +0900

    v.0.1

--- src/App.js
+++ src/App.js
@@ -1,38 +1,13 @@
 import React from "react";
-import {Route, Routes} from 'react-router-dom';
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import Main from "./pages/Main";
-import About from "./pages/About";
-import Community from "./pages/Community";
-import Gallery from "./pages/Gallery";
-import Join from "./pages/Join";
-import Login from "./pages/Login";
-import Notification from "./pages/Notification";
-import Project from "./pages/Project";
-import Section from "./pages/Section";
-import Study from "./pages/Study";
-import Recruit from "./pages/Recruit";
-import Management from "./pages/Management";
+import AppRoutes from "./\broutes/AppRoutes";
 
 const App = () => {
   return(
     <>
       <Header/>
-      <Routes>
-        <Route path ='/' element = {<Main/>}/>
-        <Route path = '/about' element={<About/>}/>
-        <Route path = '/community' element={<Community/>}/>
-        <Route path = '/gallery' element={<Gallery/>}/>
-        <Route path = '/join' element={<Join/>}/>
-        <Route path = '/login' element={<Login/>}/>
-        <Route path = '/notification' element={<Notification/>}/>
-        <Route path = '/project' element={<Project/>}/>
-        <Route path = '/section' element={<Section/>}/>
-        <Route path = '/study' element={<Study/>}/>
-        <Route path = '/recruit' element={<Recruit/>}/>
-        <Route path = '/management' element={<Management/>}/>
-      </Routes>
+        <AppRoutes/>
       <Footer/>
     </>
   );
