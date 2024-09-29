8f7916bf5f00565e660aec5db91be3ebfc31f6f3
baekcii <bsy8470@gmail.com>
2024-09-29 18:18:44 +0900

    top image

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,7 +1,19 @@
+import React, {useState, useEffect} from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import Slider from "../components/ImgSlider";
 const Main = () => {
     return(
-        <p>Main page</p>
+        <>
+        <Header/>
+        <Slider title="CPU" content="전북대학교 중앙 컴퓨터동아리  CPU"/>
+
+        </>
     );
 };
+
+
+
+
 
 export default Main;
