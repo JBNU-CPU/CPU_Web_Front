dddc5203248de374a95c1ba3c9c79fa721208030
baekcii <bsy8470@gmail.com>
2024-09-29 21:24:46 +0900

    top image

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -2,11 +2,14 @@ import React, {useState, useEffect} from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Slider from "../components/ImgSlider";
+
+
 const Main = () => {
     return(
         <>
         <Header/>
         <Slider title="CPU" content="전북대학교 중앙 컴퓨터동아리  CPU"/>
+        <Footer/>
 
         </>
     );
